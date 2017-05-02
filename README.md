252bcd773c43919959891168547bc6df7ea75c51
Mike Spreitzer <mspreitz@us.ibm.com>
2017-05-02 12:02:26 -0400

    Initial commit of example that watches ThirdPartyResource objects

---
+++ README.md
@@ -0,0 +1,8 @@
+# Intro
+
+Cloned from https://github.com/yvespp/tpr-example and updated to work
+with https://github.com/kubernetes/client-go commit
+623dbc3dc4e22ddd57b7d1d73b9afa9c9448e8f5 (which was master on or about
+April 24, 2017).
+
+Also enhanced the printing of the `Example` values.
---
+++ main.go
@@ -0,0 +1,253 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/golang/glog"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "", "Path to a kube config. Only required if out-of-cluster.")
+	flag.Parse()
+
+	// Create the client config. Use kubeconfig if given, otherwise assume in-cluster.
+	config, err := buildConfig(*kubeconfig)
+	if err != nil {
+		glog.Fatal(err.Error())
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatal(err.Error())
+		panic(err)
+	}
+
+	// initialize third party resource if it does not exist
+	tpr, err := clientset.Extensions().ThirdPartyResources().Get("example.k8s.io", metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			tpr := &v1beta1.ThirdPartyResource{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "example.k8s.io",
+				},
+				Versions: []v1beta1.APIVersion{
+					{Name: "v1"},
+				},
+				Description: "An Example ThirdPartyResource",
+			}
+
+			result, err := clientset.Extensions().ThirdPartyResources().Create(tpr)
+			if err != nil {
+				glog.Fatal(err.Error())
+				panic(err)
+			}
+			fmt.Printf("CREATED: %#v\nFROM: %#v\n", result, tpr)
+		} else {
+			glog.Fatal(err.Error())
+			panic(err)
+		}
+	} else {
+		fmt.Printf("SKIPPING: already exists %#v\n", tpr)
+	}
+
+	// make a new config for our extension's API group, using the first config as a baseline
+	var tprconfig *rest.Config
+	tprconfig = config
+	configureClient(tprconfig)
+
+	tprclient, err := rest.RESTClientFor(tprconfig)
+	if err != nil {
+		glog.Fatal(err.Error())
+		panic(err)
+	}
+
+	var example Example
+
+	err = tprclient.Get().
+		Resource("examples").
+		Namespace(api.NamespaceDefault).
+		Name("example1").
+		Do().Into(&example)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Create an instance of our TPR
+			example := &Example{
+				Metadata: v1.ObjectMeta{
+					Name: "example1",
+				},
+				Spec: ExampleSpec{
+					Foo: "hello",
+					Bar: true,
+				},
+			}
+
+			var result Example
+			err = tprclient.Post().
+				Resource("examples").
+				Namespace(api.NamespaceDefault).
+				Body(example).
+				Do().Into(&result)
+
+			if err != nil {
+				glog.Fatal(err.Error())
+				glog.Fatal(err)
+				panic(err)
+			}
+			fmt.Printf("CREATED: %#v\n", result)
+		} else {
+			glog.Fatal(err.Error())
+			panic(err)
+		}
+	} else {
+		fmt.Printf("GET: %#v\n", example)
+	}
+
+	// Fetch a list of our TPRs
+	exampleList := ExampleList{}
+	err = tprclient.Get().Resource("examples").Do().Into(&exampleList)
+	if err != nil {
+		glog.Fatal(err.Error())
+		panic(err)
+	}
+	fmt.Printf("LIST: %#v\n", exampleList)
+
+	fmt.Printf("Starting watch!\n")
+	watch(tprclient)
+}
+
+func buildConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	return rest.InClusterConfig()
+}
+
+func configureClient(config *rest.Config) {
+	groupversion := schema.GroupVersion{
+		Group:   "k8s.io",
+		Version: "v1",
+	}
+
+	config.GroupVersion = &groupversion
+	config.APIPath = "/apis"
+	config.ContentType = runtime.ContentTypeJSON
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: api.Codecs}
+
+	schemeBuilder := runtime.NewSchemeBuilder(
+		func(scheme *runtime.Scheme) error {
+			scheme.AddKnownTypes(
+				groupversion,
+				&Example{},
+				&ExampleList{},
+				&v1.ListOptions{},
+				&v1.DeleteOptions{},
+			)
+			return nil
+		})
+	schemeBuilder.AddToScheme(api.Scheme)
+}
+
+func watch(client *rest.RESTClient) {
+
+	stop := make(chan struct{}, 1)
+	source := cache.NewListWatchFromClient(
+		client,
+		"examples",
+		api.NamespaceAll,
+		fields.Everything())
+
+	store, controller := cache.NewInformer(
+		source,
+
+		// The object type.
+		&Example{},
+
+		// resyncPeriod
+		// Every resyncPeriod, all resources in the cache will retrigger events.
+		// Set to 0 to disable the resync.
+		time.Second*60,
+
+		// Your custom resource event handlers.
+		cache.ResourceEventHandlerFuncs{
+			// Takes a single argument of type interface{}.
+			// Called on controller startup and when new resources are created.
+			AddFunc: create,
+
+			// Takes two arguments of type interface{}.
+			// Called on resource update and every resyncPeriod on existing resources.
+			UpdateFunc: update,
+
+			// Takes a single argument of type interface{}.
+			// Called on resource deletion.
+			DeleteFunc: delete,
+		})
+
+	// store can be used to List and Get
+	// NEVER modify objects from the store. It's a read-only, local cache.
+	fmt.Println("listing examples from store:")
+	for _, obj := range store.List() {
+		example := obj.(*Example)
+
+		// This will likely be empty the first run, but may not
+		fmt.Printf("%#v\n", example)
+	}
+
+	// the controller run starts the event processing loop
+	go controller.Run(stop)
+
+	// and now we block on a signal
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	s := <-signals
+	fmt.Printf("received signal %#v, exiting...\n", s)
+	close(stop)
+	os.Exit(0)
+}
+
+// Handler functions as per the controller above.
+// Note the coercion of the interface{} into a pointer of the expected type.
+
+func create(obj interface{}) {
+	example := obj.(*Example)
+
+	fmt.Println("CREATED:", printExample(example))
+}
+
+func update(old, new interface{}) {
+	oldExample := old.(*Example)
+	newExample := new.(*Example)
+
+	fmt.Printf("UPDATED:\n  old: %s\n  new: %s\n", printExample(oldExample), printExample(newExample))
+}
+
+func delete(obj interface{}) {
+	example := obj.(*Example)
+
+	fmt.Println("DELETED:", printExample(example))
+}
+
+// convenience functions
+func printExample(example *Example) string {
+	return fmt.Sprintf("%s/%s, APIVersion: %s, Kind: %s, Value: %#v", example.Metadata.Namespace, example.Metadata.Name, example.APIVersion, example.Kind, example)
+}
